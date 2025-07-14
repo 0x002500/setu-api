e8dfc8fba810e4e6d973a1f528d1f65365af49d4
0x002500 <tiger_pan@outlook.com>
2025-07-14 17:23:18 +0800

    Initial commit

---
+++ src/hex2color.rs
@@ -0,0 +1,23 @@
+use crate::Color;
+
+pub fn hex2color(hex: &str) -> Color {
+    let hex = hex.trim_start_matches('#');
+    let len = hex.len();
+
+    let r: u8 = u8::from_str_radix(&hex[0..2], 16).unwrap_or(0);
+    let g: u8 = u8::from_str_radix(&hex[2..4], 16).unwrap_or(0);
+    let b: u8 = u8::from_str_radix(&hex[4..6], 16).unwrap_or(0);
+
+    let a: u8 = if len == 8 {
+        u8::from_str_radix(&hex[6..8], 16).unwrap_or(255)
+    } else {
+        255
+    };
+
+    Color {
+        r: r,
+        g: g,
+        b: b,
+        a: a,
+    }
+}
