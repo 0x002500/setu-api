0a80ad9c116e4b4fa7088ff16ee5acc08c7554e6
0x002500 <tiger_pan@outlook.com>
2025-07-14 17:30:19 +0800

    feat: add actix_web main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-mod hex2color;
-mod generate_pictures;
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 
-pub struct Color{
+mod generate_pictures;
+mod hex2color;
+
+pub struct Color {
     r: u8,
     g: u8,
     b: u8,
-    a: u8
+    a: u8,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new())
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
