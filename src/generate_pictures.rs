86c6255dd52b6dfd3dd9a517f12f0da6e2bce8e2
0x002500 <tiger_pan@outlook.com>
2025-07-26 17:36:50 +0800

    fix: make generate_pictures function public

--- src/generate_pictures.rs
+++ src/generate_pictures.rs
@@ -3,7 +3,7 @@ use std::io::Cursor;
 
 use crate::{Color, Size};
 
-async fn generate_pictures(color: Color, size: Size) -> Vec<u8>{
+pub async fn generate_pictures(color: Color, size: Size) -> Vec<u8>{
     let img: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::<Rgba<u8>, _>::from_fn(size.width, size.height, |_x: u32, _y: u32| Rgba([color.r, color.g, color.b, color.a]));
 
     let mut buf: Cursor<Vec<u8>> = Cursor::new(Vec::new());
