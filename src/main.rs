7be5edb3eb65392ab137d5e71bc182c5c0d456b0
0x002500 <tiger_pan@outlook.com>
2025-08-05 20:20:15 +0800

    feat: add /pic route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use serde::Deserialize;
+use crate::{generate_pictures::generate_pictures, hex2color::hex2color};
 
 mod generate_pictures;
 mod hex2color;
@@ -15,11 +17,25 @@ pub struct Size {
     height: u32
 }
 
+#[derive(Deserialize)]
+struct Pic {
+    color: String,
+    width: u32,
+    height: u32
+}
+
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Thanks for using setu-api!")
 }
 
+#[get("/pic")]
+async fn pic(pic: web::Query<Pic>) -> impl Responder {
+    let size: Size = Size { width: pic.width, height: pic.height };
+    let color: Color = hex2color(&pic.color);
+    let image_bytes: Vec<u8> = generate_pictures(color, size).await;
+    HttpResponse::Ok().body(image_bytes)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
